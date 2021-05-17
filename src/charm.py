5e3b9c95042594e70ba32a32070361c68acd27a1
Colin Misare <colin.misare@canonical.com>
2021-05-17 01:05:20 -0600

    further development of the charm

--- src/charm.py
+++ src/charm.py
@@ -16,13 +16,17 @@
 
 import logging
 
+from redis import Redis
+from redis.exceptions import RedisError
+
 from charms.redis_k8s.v0.redis import RedisProvides
 from ops.charm import CharmBase
 from ops.main import main
-from ops.model import ActiveStatus
+from ops.model import ActiveStatus, WaitingStatus
+from ops.pebble import ConnectionError
 
-# We expect the redis container to use the default port
-DEFAULT_PORT = 6379
+REDIS_PORT = 6379
+WAITING_MESSAGE = 'Waiting for Redis...'
 
 logger = logging.getLogger(__name__)
 
@@ -30,22 +34,17 @@ logger = logging.getLogger(__name__)
 class RedisK8sCharm(CharmBase):
     def __init__(self, *args):
         super().__init__(*args)
-        logger.debug('Initializing charm')
 
-        self.redis_provides = RedisProvides(self, port=DEFAULT_PORT)
+        self.redis_provides = RedisProvides(self, port=REDIS_PORT)
 
-        self.framework.observe(self.on.start, self.configure_pod)
-        self.framework.observe(self.on.config_changed, self.configure_pod)
-        self.framework.observe(self.on.upgrade_charm, self.configure_pod)
+        self.framework.observe(self.on.config_changed, self.config_changed)
+        self.framework.observe(self.on.upgrade_charm, self.config_changed)
+        self.framework.observe(self.on.update_status, self.update_status)
 
-    def configure_pod(self, event):
-        """Applies the pod configuration.
-        """
-        if not self.unit.is_leader():
-            logger.info("Spec changes ignored by non-leader")
-            self.unit.status = ActiveStatus()
-            return
+        self.framework.observe(self.on.check_service_action, self.check_service)
 
+    def config_changed(self, event):
+        logger.info("Beginning config_changed")
         layer_config = {
             "summary": "Redis layer",
             "description": "Redis layer",
@@ -62,21 +61,55 @@ class RedisK8sCharm(CharmBase):
             }
         }
 
-        container = self.unit.get_container("redis")
-        services = container.get_plan().to_dict().get("services", {})
+        try:
+            container = self.unit.get_container("redis")
+            services = container.get_plan().to_dict().get("services", {})
 
-        if services != layer_config["services"]:
-            logger.debug("About to add_layer with layer_config:\n{}".format(layer_config))
-            container.add_layer("redis", layer_config, combine=True)
+            if services != layer_config["services"]:
+                logger.debug("About to add_layer with layer_config:\n{}".format(layer_config))
+                container.add_layer("redis", layer_config, combine=True)
 
-            service = container.get_service("redis")
-            if service.is_running():
-                logger.debug("Stopping service")
-                container.stop("redis")
-            logger.debug("Starting service")
-            container.start("redis")
+                service = container.get_service("redis")
+                if service.is_running():
+                    logger.debug("Stopping service")
+                    container.stop("redis")
+                logger.debug("Starting service")
+                container.start("redis")
+        except ConnectionError:
+            logger.info("Pebble is not ready, deferring event")
+            event.defer()
+            return
 
-        self.unit.status = ActiveStatus()
+        self._redis_check()
+
+    def update_status(self, event):
+        logger.info("Beginning update_status")
+        self._redis_check()
+
+    def check_service(self, event):
+        logger.info("Beginning check_service")
+        results = {}
+        if self._redis_check():
+            results["result"] = "Service is running"
+        else:
+            results["result"] = "Service is not running"
+        event.set_results(results)
+
+    def _redis_check(self):
+        try:
+            redis = Redis()
+            info = redis.info("server")
+            version = info["redis_version"]
+            self.unit.status = ActiveStatus()
+            if self.unit.is_leader():
+                self.app.status = ActiveStatus()
+                self.unit.set_workload_version(version)
+            return True
+        except RedisError:
+            self.unit.status = WaitingStatus(WAITING_MESSAGE)
+            if self.unit.is_leader():
+                self.app.status = WaitingStatus(WAITING_MESSAGE)
+            return False
 
 
 if __name__ == "__main__":  # pragma: nocover
--- tests/test_charm.py
+++ tests/test_charm.py
@@ -15,8 +15,11 @@
 
 from unittest import TestCase, mock
 
-from ops.model import ActiveStatus, Container
-from ops.pebble import ServiceInfo
+from redis import Redis
+from redis.exceptions import RedisError
+
+from ops.model import ActiveStatus, Container, MaintenanceStatus, UnknownStatus, WaitingStatus
+from ops.pebble import ServiceInfo, ConnectionError
 from ops.testing import Harness
 
 from charm import RedisK8sCharm
@@ -29,41 +32,50 @@ class TestCharm(TestCase):
         self.addCleanup(self.harness.cleanup)
         self.harness.begin()
 
-    def test_on_start_when_unit_is_not_leader(self):
-        # Given
-        self.harness.set_leader(False)
-        # When
-        self.harness.charm.on.start.emit()
-        # Then
-        self.assertEqual(
-            self.harness.charm.unit.status,
-            ActiveStatus()
-        )
-
-    def test_on_config_changed_when_unit_is_not_leader(self):
-        # Given
-        self.harness.set_leader(False)
-        # When
-        self.harness.update_config()
-        # Then
-        self.assertEqual(
-            self.harness.charm.unit.status,
-            ActiveStatus()
-        )
-
-    def test_on_upgrade_charm_when_unit_is_not_leader(self):
-        # Given
-        self.harness.set_leader(False)
-        # When
-        self.harness.charm.on.upgrade_charm.emit()
-        # Then
-        self.assertEqual(
-            self.harness.charm.unit.status,
-            ActiveStatus()
-        )
-
-    def test_config_changed_when_unit_is_leader(self):
+    @mock.patch.object(Redis, "info")
+    def test_on_update_status_success_leader(self, info):
         self.harness.set_leader(True)
+        info.return_value = {"redis_version": "6.0.11"}
+        self.harness.charm.on.update_status.emit()
+        self.assertEqual(self.harness.charm.unit.status, ActiveStatus())
+        self.assertEqual(self.harness.charm.app.status, ActiveStatus())
+        self.assertEqual(self.harness.get_workload_version(), "6.0.11")
+
+    @mock.patch.object(Redis, "info")
+    def test_on_update_status_failure_leader(self, info):
+        self.harness.set_leader(True)
+        info.side_effect = RedisError("Error connecting to redis")
+        self.harness.charm.on.update_status.emit()
+        self.assertEqual(self.harness.charm.unit.status, WaitingStatus("Waiting for Redis..."))
+        self.assertEqual(self.harness.charm.app.status, WaitingStatus("Waiting for Redis..."))
+        self.assertEqual(self.harness.get_workload_version(), None)
+
+    @mock.patch.object(Redis, "info")
+    def test_on_update_status_success_not_leader(self, info):
+        self.harness.set_leader(False)
+        info.return_value = {"redis_version": "6.0.11"}
+        self.harness.charm.on.update_status.emit()
+        self.assertEqual(self.harness.charm.unit.status, ActiveStatus())
+        # Without setting back to leader, the below throws a RuntimeError on app.status
+        self.harness.set_leader(True)
+        self.assertEqual(self.harness.charm.app.status, UnknownStatus())
+        self.assertEqual(self.harness.get_workload_version(), None)
+
+    @mock.patch.object(Redis, "info")
+    def test_on_update_status_failure_not_leader(self, info):
+        self.harness.set_leader(False)
+        info.side_effect = RedisError("Error connecting to redis")
+        self.harness.charm.on.update_status.emit()
+        self.assertEqual(self.harness.charm.unit.status, WaitingStatus("Waiting for Redis..."))
+        # Without setting back to leader, the below throws a RuntimeError on app.status
+        self.harness.set_leader(True)
+        self.assertEqual(self.harness.charm.app.status, UnknownStatus())
+        self.assertEqual(self.harness.get_workload_version(), None)
+
+    @mock.patch.object(Redis, "info")
+    def test_config_changed_when_unit_is_leader_status_success(self, info):
+        self.harness.set_leader(True)
+        info.return_value = {"redis_version": "6.0.11"}
         self.harness.update_config()
         found_plan = self.harness.get_container_pebble_plan("redis").to_dict()
         expected_plan = {
@@ -83,13 +95,60 @@ class TestCharm(TestCase):
         container = self.harness.model.unit.get_container("redis")
         service = container.get_service("redis")
         self.assertTrue(service.is_running())
-        self.assertEqual(
-            self.harness.charm.unit.status,
-            ActiveStatus()
-        )
+        self.assertEqual(self.harness.charm.unit.status, ActiveStatus())
+        self.assertEqual(self.harness.charm.app.status, ActiveStatus())
+        self.assertEqual(self.harness.get_workload_version(), "6.0.11")
 
-    def test_on_start_when_unit_is_leader_and_service_running(self):
+    @mock.patch.object(Redis, "info")
+    def test_config_changed_when_unit_is_leader_status_failure(self, info):
         self.harness.set_leader(True)
+        info.side_effect = RedisError("Error connecting to redis")
+        self.harness.update_config()
+        found_plan = self.harness.get_container_pebble_plan("redis").to_dict()
+        expected_plan = {
+            "services": {
+                "redis": {
+                    "override": "replace",
+                    "summary": "Redis service",
+                    "command": "/usr/local/bin/start-redis.sh redis-server",
+                    "startup": "enabled",
+                    "environment": {
+                        "ALLOW_EMPTY_PASSWORD": "yes"
+                    }
+                }
+            }
+        }
+        self.assertEqual(found_plan, expected_plan)
+        container = self.harness.model.unit.get_container("redis")
+        service = container.get_service("redis")
+        self.assertTrue(service.is_running())
+        self.assertEqual(self.harness.charm.unit.status, WaitingStatus("Waiting for Redis..."))
+        self.assertEqual(self.harness.charm.app.status, WaitingStatus("Waiting for Redis..."))
+        self.assertEqual(self.harness.get_workload_version(), None)
+
+    @mock.patch.object(Redis, "info")
+    def test_config_changed_pebble_error(self, info):
+        self.harness.set_leader(True)
+        mock_container = mock.MagicMock(Container)
+        mock_container.get_plan.side_effect = ConnectionError("Error connecting to pebble")
+
+        def mock_get_container(name):
+            return mock_container
+
+        self.harness.model.unit.get_container = mock_get_container
+        self.harness.update_config()
+        mock_container.add_layer.assert_not_called()
+        mock_container.stop.assert_not_called()
+        mock_container.start.assert_not_called()
+        self.assertEqual(self.harness.charm.unit.status, MaintenanceStatus(""))
+        self.assertEqual(self.harness.charm.app.status, UnknownStatus())
+        self.assertEqual(self.harness.get_workload_version(), None)
+        # TODO - test for the event being deferred
+
+    @mock.patch.object(Redis, "info")
+    def test_config_changed_when_unit_is_leader_and_service_is_running(self, info):
+        self.harness.set_leader(True)
+        info.return_value = {"redis_version": "6.0.11"}
         mock_info = {
             "name": "redis",
             "startup": "enabled",
@@ -106,20 +165,23 @@ class TestCharm(TestCase):
         self.harness.update_config()
         mock_container.stop.assert_called_once_with("redis")
         mock_container.start.assert_called_once_with("redis")
+        self.assertEqual(self.harness.charm.unit.status, ActiveStatus())
+        self.assertEqual(self.harness.charm.app.status, ActiveStatus())
+        self.assertEqual(self.harness.get_workload_version(), "6.0.11")
 
-    @mock.patch.object(RedisProvides, '_bind_address')
+    @mock.patch.object(RedisProvides, "_bind_address")
     def test_on_relation_changed_status_when_unit_is_leader(self, bind_address):
         # Given
         self.harness.set_leader(True)
-        bind_address.return_value = '10.2.1.5'
+        bind_address.return_value = "10.2.1.5"
 
-        rel_id = self.harness.add_relation('redis', 'wordpress')
-        self.harness.add_relation_unit(rel_id, 'wordpress/0')
+        rel_id = self.harness.add_relation("redis", "wordpress")
+        self.harness.add_relation_unit(rel_id, "wordpress/0")
         # When
-        self.harness.update_relation_data(rel_id, 'wordpress/0', {})
+        self.harness.update_relation_data(rel_id, "wordpress/0", {})
         rel_data = self.harness.get_relation_data(
             rel_id, self.harness.charm.unit.name
         )
         # Then
-        self.assertEqual(rel_data.get('hostname'), '10.2.1.5')
-        self.assertEqual(rel_data.get('port'), '6379')
+        self.assertEqual(rel_data.get("hostname"), "10.2.1.5")
+        self.assertEqual(rel_data.get("port"), "6379")
