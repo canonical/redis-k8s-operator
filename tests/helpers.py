e61a4bf99e3b3bbebb6a4f9377f42d68f21f5aea
Raúl Zamora Martínez <76525382+zmraul@users.noreply.github.com>
2022-04-26 10:28:38 +0200

    DPE-214 user defined tls certificates (#27)
    * add TLS support as a resource
    
    * add tls resource handling and config option to enable/disable

---
+++ tests/helpers.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# Copyright 2022 Canonical Ltd.
+# See LICENSE file for licensing details.
+
+
+from pathlib import Path
+
+import yaml
+
+METADATA = yaml.safe_load(Path("./metadata.yaml").read_text())
+APP_NAME = METADATA["name"]
+STORAGE_PATH = METADATA["storage"]["database"]["location"]
+TLS_RESOURCES = {
+    "cert-file": "tests/tls/redis.crt",
+    "key-file": "tests/tls/redis.key",
+    "ca-cert-file": "tests/tls/ca.crt",
+}
