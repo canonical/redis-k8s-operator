8dad8cc7a270609b938896373861a905f53e6c19
Raul Zamora <raul.zamora@canonical.com>
2022-06-08 16:19:12 +0200

    add replication basics

---
+++ src/literals.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# This file is part of the Redis k8s Charm for Juju.
+# Copyright 2022 Canonical Ltd.
+
+"""Literals used by the Redis charm."""
+
+WAITING_MESSAGE = "Waiting for Redis..."
+PEER = "redis-peers"
+PEER_PASSWORD_KEY = "redis-password"
+LEADER_HOST_KEY = "leader-host"
+
+REDIS_PORT = 6379
+
+CONFIG_PATH = "/etc/redis/"
+REDIS_CONFIG_PATH = f"{CONFIG_PATH}/redis.conf"
