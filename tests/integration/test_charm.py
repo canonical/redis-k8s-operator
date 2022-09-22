48d63b2db32804383146c47e342557e9030a36c1
Raul Zamora <raul.zamora@canonical.com>
2022-09-22 16:35:18 +0200

    extend waiting time after failover command

--- tests/integration/test_charm.py
+++ tests/integration/test_charm.py
@@ -280,7 +280,7 @@ async def test_scale_up_replication_after_failover(ops_test: OpsTest):
     sentinel = Redis(leader_address, password=sentinel_password, port=26379, decode_responses=True)
     sentinel.execute_command(f"SENTINEL failover {APP_NAME}")
     # Give time so sentinel updates information of failover
-    time.sleep(10)
+    time.sleep(60)
 
     await ops_test.model.block_until(
         lambda: "failover-status" not in sentinel.execute_command(f"SENTINEL MASTER {APP_NAME}"),
@@ -348,7 +348,7 @@ async def test_scale_down_departing_master(ops_test: OpsTest):
     # Failover so the last unit becomes master
     sentinel.execute_command(f"SENTINEL FAILOVER {APP_NAME}")
     # Give time so sentinel updates information of failover
-    time.sleep(10)
+    time.sleep(60)
 
     await ops_test.model.block_until(
         lambda: "failover-status" not in sentinel.execute_command(f"SENTINEL MASTER {APP_NAME}"),
